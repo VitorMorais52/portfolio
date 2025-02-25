b2ccc1970dcfa8dd9118167143f445db1ec3933f
Morais <vitormorais52@gmail.com>
2025-02-24 22:56:41 -0300

    adding functionality to input filter

--- src/app/_components/pages/ecommerce/InputFilter.tsx
+++ src/app/_components/pages/ecommerce/InputFilter.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { useState } from "react";
+import { useFilters } from "src/app/_lib/contexts/useProductsFilter";
+
 export default function InputFilter() {
+  const { name, setName } = useFilters();
   return (
     <input
       type="text"
       placeholder="Search"
+      value={name}
+      onChange={({ target }) => setName(target.value)}
       className="mt-6 border-2 border-solid border-[#585858] rounded-[12px] text-[#E7E7E7] bg-transparent p-1 pl-2"
     />
   );
