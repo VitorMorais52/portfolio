b157f1c53201b4007995853cadb8bb28a89f43cd
Morais <vitormorais52@gmail.com>
2025-03-03 02:59:18 -0300

    adding google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
+
 const inter = Inter({ subsets: ["latin"] });
 
 import "../ui/global.css";
@@ -11,11 +13,27 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-HW5FDSE4VP"
+        />
+
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: ` window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HW5FDSE4VP');`,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useMemo } from "react";
+import { useEffect } from "react";
 
 import Header from "./_components/common/header";
 import Footer from "./_components/common/footer";
@@ -10,15 +10,18 @@ const pagesName = ["home", "blog", "institutional", "ecommerce", "resume"];
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const currentPage = useMemo(() => {
-    const regex = "/([^/]+)";
-    const match = pathname.match(regex);
-    return match ? match[1] : "home";
+
+  useEffect(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("config", "G-XXXXXXXXXX", {
+        page_path: pathname,
+      });
+    }
   }, [pathname]);
 
   return (
     <div className="overflow-x-hidden mb-8">
-      <Header currentPage={currentPage} pagesName={pagesName} />
+      <Header currentPage={pathname} pagesName={pagesName} />
       {children}
       <Footer pagesName={pagesName} />
     </div>
