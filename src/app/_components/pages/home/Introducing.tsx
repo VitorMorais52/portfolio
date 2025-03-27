352ba1a2fa5048661e782a340dc4a36f404256a8
Morais <vitormorais52@gmail.com>
2025-03-26 23:33:45 -0300

    updating introducing component behavior

--- src/app/_components/pages/home/Introducing.tsx
+++ src/app/_components/pages/home/Introducing.tsx
@@ -3,14 +3,23 @@
 import { useState } from "react";
 import { Mark, PointingFinger } from "../../common/icons";
 
+const previousAnswer = localStorage.getItem("@mt-portfolio/whoAreYou") || null;
 export default function Introducing() {
   const [isHovered, setIsHovered] = useState(false);
   const [loadingButton, setLoadingButton] = useState<string | null>(null);
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [selectedOption, setSelectedOption] = useState<string | null>(
+    previousAnswer
+  );
+
+  console.log(selectedOption);
 
   const handleSelect = (option: string) => {
+    if (selectedOption) return;
+
     setLoadingButton(option);
     setSelectedOption(option);
+    localStorage.setItem("@mt-portfolio/whoAreYou", option);
+
     window.gtag("event", "home_events", {
       event_category: "who are you?",
       event_label: option,
@@ -20,8 +29,6 @@ export default function Introducing() {
       setLoadingButton("check-" + option);
       setTimeout(() => {
         setLoadingButton(null);
-        setIsHovered(false);
-        setSelectedOption(null);
       }, 1000);
     }, 1200);
   };
@@ -29,7 +36,9 @@ export default function Introducing() {
   return (
     <div
       className="overflow-hidden relative w-full flex flex-col items-center h-[36px]"
-      onMouseLeave={() => setIsHovered(false)}
+      onMouseLeave={() => {
+        if (!selectedOption) setIsHovered(false);
+      }}
     >
       <span
         className={`cursor-pointer mt-[6px] absolute transition-transform duration-300 ease-in-out ${
@@ -54,14 +63,23 @@ export default function Introducing() {
         ].map((option) => (
           <button
             key={option.value}
-            className="rounded-md px-4 py-2 text-sm hover:text-gray-700 hover:bg-gray-100 flex items-center justify-center h-[36px] w-[92px]"
+            className={`rounded-md px-4 py-2 text-sm  flex items-center justify-center h-[36px] w-[92px] 
+              ${!selectedOption ? "hover:text-gray-700 hover:bg-gray-100" : ""}
+              ${
+                selectedOption === option.value
+                  ? "text-gray-700 bg-gray-100"
+                  : ""
+              }
+            `}
             onClick={() => handleSelect(option.value)}
             disabled={selectedOption !== null}
           >
             {loadingButton === option.value ? (
               <div className="animate-spin w-4 h-4 border-2 border-gray-500 border-t-transparent rounded-full" />
             ) : loadingButton === "check-" + option.value ? (
-              <Mark className="w-[26px] h-[26px]" />
+              <span className="flex items-center justify-center ml-[13px]">
+                Sent <Mark className="w-[26px] h-[26px] mb-1" />
+              </span>
             ) : (
               option.label
             )}
